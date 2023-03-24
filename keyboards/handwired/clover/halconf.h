bde4a8a5b58810e4babf9cbb1805f548fa457416
Gary Glover <light.bed5489@fourleafclover.uk>
2023-03-24 13:41:25 +0000

    OLED and Trackball

--- keyboards/handwired/clover/halconf.h
+++ keyboards/handwired/clover/halconf.h
@@ -4,7 +4,17 @@
 #pragma once
 
 #define HAL_USE_I2C TRUE
-#define HAL_USE_PWM TRUE
-#define HAL_USE_ADC TRUE
+#define HAL_USE_SPI TRUE
+//#define HAL_USE_PWM TRUE
+//#define HAL_USE_ADC TRUE
+
+#define I2C_DRIVER I2CD1
+#define I2C1_SCL_PIN GP27
+#define I2C1_SDA_PIN GP26
+
+#define SPI_DRIVER SPID0
+#define SPI_SCK_PIN GP18
+#define SPI_MISO_PIN GP16
+#define SPI_MOSI_PIN GP19
 
 #include_next <halconf.h>
