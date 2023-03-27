37faf4f5d128283bc1fd5910f310d474287135bf
Gary Glover <light.bed5489@fourleafclover.uk>
2023-03-27 09:23:54 +0100

    Layout & feature improvements

--- keyboards/handwired/clover/halconf.h
+++ keyboards/handwired/clover/halconf.h
@@ -9,11 +9,14 @@
 //#define HAL_USE_ADC TRUE
 
 #define I2C_DRIVER I2CD1
+#undef I2C1_SCL_PIN
 #define I2C1_SCL_PIN GP27
+#undef I2C1_SDA_PIN
 #define I2C1_SDA_PIN GP26
 
 #define SPI_DRIVER SPID0
 #define SPI_SCK_PIN GP18
+#undef SPI_MISO_PIN
 #define SPI_MISO_PIN GP16
 #define SPI_MOSI_PIN GP19
 
