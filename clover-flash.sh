f2c802c494129bd635e8e4f855676504a7c43b7a
Gary Glover <light.bed5489@fourleafclover.uk>
2023-05-22 08:38:45 +0100

    Attempt at chording layout

--- clover-flash.sh
+++ clover-flash.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 qmk flash -kb handwired/clover -km default -bl uf2-split-left
-qmk flash -kb handwired/clover -km default -bl uf2-split-right
+#qmk flash -kb handwired/clover -km default -bl uf2-split-right
